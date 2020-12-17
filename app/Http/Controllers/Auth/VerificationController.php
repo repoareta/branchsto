1e9ff402aec948906759ab78fe4e50f08a2bb1f5
mulyono <37521871+mulyonomuiz63@users.noreply.github.com>
2020-12-18 06:21:25 +0700

    edit search riding class

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = 'competitions/data-competitions';
 
     public function verifikasi()
     {
