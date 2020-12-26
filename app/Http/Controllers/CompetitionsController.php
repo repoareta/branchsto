07925f3543fd8d4ad1e7c6425909d3eef6649384
mulyono <37521871+mulyonomuiz63@users.noreply.github.com>
2020-12-26 17:08:41 +0700

    Expired

--- app/Http/Controllers/CompetitionsController.php
+++ app/Http/Controllers/CompetitionsController.php
@@ -8,24 +8,6 @@ class CompetitionsController extends Controller
 {
     public function index()
     {
-        foreach(DB::table('bookings')->where('photo', null)->where('approval_status', null)->get() as $item)
-        {
-            $time_start = strtotime($item->created_at);
-            $time_end   = strtotime(now());
-
-            //menghitung selisih dengan hasil detik
-            $diff    =$time_end - $time_start;
-            if ($diff > 3600) {
-                DB::table('bookings')->where('id',$item->id)
-                ->update([
-                    'approval_status' => "Expired"
-                ]);
-                foreach(DB::table('booking_details')->where('booking_id',$item->id)->get() as $row)
-                {
-                    DB::table('slot_user')->where('booking_detail_id',$row->id)->delete();
-                }
-            }
-        }
         return view('home.index');
     }
 }
